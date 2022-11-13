a200a8124076c71950659ca5355f8e6e50334d5f
Maxim Azarov <71723565+Leo-D-Neo@users.noreply.github.com>
2022-11-13 12:02:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ import matplotlib.pyplot as plt # package for plot function
 
 
 class Gabor3D:
-    def __init__(self, size, theta, w_t0, w, sigma):
+    def __init__(self, size_xy, size_deep, theta, w_t0, w, sigma_xy, sigma_t):
         """
-        :param size: размер фильтра (по всем осям одинаков), задается нечетным числом
+        :param size_xy: размер фильтра по осям x и y, задается нечетным числом
+        :param size_deep: размер фильтра по оси t, задается нечетным числом
         :param theta: угол поворота фильтра (в радианах)
-        :param w_t0: частота по оси t ??????????
-        :param w: пространственная частота  ????????????
-        :param sigma: ско функции Гаусса ?????????????? (по идее зависит от size)
+        :param w_t0: частота по оси t
+        :param w: пространственная частота
+        :param sigma: ско функции Гаусса
         """
-        sigma_x = sigma
-        sigma_y = sigma
-        sigma_t = sigma
 
-        radius = size // 2
-        array_axis = range( -radius, radius + 1)
-        x, y, t = np.meshgrid(array_axis, array_axis, array_axis)
+        sigma_x = sigma_xy
+        sigma_y = sigma_xy
+
+        radius_xy = size_xy // 2
+        radius_deep = size_deep // 2
+        array_axis_xy = range( -radius_xy, radius_xy + 1)
+        array_axis_t = range(-radius_deep, radius_deep + 1)
+        t, y, x = np.meshgrid(array_axis_t, array_axis_xy, array_axis_xy, indexing='ij')
 
         w_x0 = w * np.cos(theta)
         w_y0 = w * np.sin(theta)
@@ -34,12 +37,12 @@ class Gabor3D:
 
     def _convolution3D(self, img_blocks, filter3D):
         # предполагается что длина фильтра по z равна длине блока изб по z
-        size_z = filter3D.shape[2]
+        size_z = filter3D.shape[0]
 
-        out_img = np.zeros_like(img_blocks[:, :, 0])
+        out_img = np.zeros_like(img_blocks[0])
 
         for i in range(size_z):
-            out_img = out_img + cv2.filter2D(img_blocks[:, :, i], -1, filter3D[:, :, i])
+            out_img = out_img + cv2.filter2D(img_blocks[i], -1, filter3D[i])
 
         return out_img
 
@@ -50,48 +53,51 @@ class Gabor3D:
         return odd_img**2 + even_img**2
 
 
+def show_filter3D(filter3D, size_ax_x, along_axis_show = "t"):
+    if len(along_axis_show) == 1:
+        _show_filter3D(filter3D, size_ax_x, along_axis_show)
+        plt.show()
+    else:
+        for i, ch in enumerate(along_axis_show):
+            _show_filter3D(filter3D, size_ax_x[i], ch)
+        plt.show()
 
 
+def _show_filter3D(filter3D, size_ax_x, along_axis_show = "t"):
+    if along_axis_show == "t":
+        len_filter = filter3D.shape[0]
+    elif along_axis_show == "x":
+        len_filter = filter3D.shape[2]
+    elif along_axis_show == "y":
+        len_filter = filter3D.shape[1]
 
+    size_ax_y = len_filter // size_ax_x
+    if len_filter % size_ax_x != 0:
+        size_ax_y += 1
 
-def show_filter3D(filter3D, along_axis_show = "t"):
-    size_filter = filter3D.shape[0]
-    size_ax_x = 10
-    size_ax_y = size_filter // size_ax_x  + 1
-    fig, axes = plt.subplots(size_ax_y, size_ax_x)
+    fig = plt.figure()
+    fig.canvas.manager.set_window_title('Axis ' + along_axis_show)
 
-    i = 0
-    for i in range(size_filter):
-        y = i // size_ax_x
-        x = i % size_ax_x
-
+    for i in range(len_filter):
         if along_axis_show == "t":
-            g_2d = filter3D[:, :, i]
+            g_2d = filter3D[i]
         elif along_axis_show == "x":
-            g_2d = filter3D[i, :, :]
+            g_2d = filter3D[:, :, i]
         elif along_axis_show == "y":
             g_2d = filter3D[:, i, :]
 
-        ax = axes[y][x]
-        ax.imshow(g_2d, cmap=plt.gray())
-        ax.set_title(str(i))
-        ax.set_xticks([])
-        ax.set_yticks([])
+        plt.subplot(size_ax_y, size_ax_x, i + 1)
+        plt.imshow(g_2d, cmap=plt.gray())
+        plt.title(str(i))
+        plt.xticks([]), plt.yticks([])
+        print(f"g_2d(ax = {along_axis_show},n = {i}) = ", [np.min(g_2d), np.max(g_2d)], g_2d.dtype)
 
-    for j in range(i+1, size_ax_y * size_ax_x):
-        y = j // size_ax_x
-        x = j % size_ax_x
-        fig.delaxes(axes[y][x])
 
-    fig.set_figwidth(12)  # ширина и
-    fig.set_figheight(6)  # высота "Figure"
-
-    plt.show()
 
 
 def show_images(list_img, names, size_ax_x):
     size_ax_y = len(list_img) // size_ax_x
-    if len(list_img) % size_ax_x != 0 :
+    if len(list_img) % size_ax_x != 0:
         size_ax_y += 1
 
     for i in range(len(list_img)):
@@ -103,60 +109,71 @@ def show_images(list_img, names, size_ax_x):
         print(names[i], "=", [np.min(img), np.max(img)], img.dtype)
     plt.show()
 
-def main():
 
+def main():
     cap = cv2.VideoCapture("test.avi")
 
-    n = 15  # глубина блока изб и фильтра, должна быть нечетной
+    n = 7  # глубина блока изб и фильтра, должна быть нечетной
 
-    ret, img = cap.read()
-    block_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    block_img = []
 
     # создаем блок изб
-    for i in range(n-1):
+    for i in range(n):
         ret, img = cap.read()
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        block_img = np.dstack((block_img, gray_img))
+        block_img.append(gray_img)
+    block_img = np.stack(block_img, axis=0)
 
-    gabor_filter = Gabor3D(size=n, theta=0, w_t0=0.4, w=0.24, sigma=2.1)
+    gabor_filter = Gabor3D(size_xy=25, size_deep=n, theta=np.radians(35), w_t0=1/7, w=1/4, sigma_xy=4, sigma_t=1)
 
-    show_filter3D(gabor_filter.odd_filter, "t")
+    show_filter3D(gabor_filter.even_filter, [10,5], "xt")
 
-    res_img = gabor_filter.filtered(block_img)
-
-    cv2.imshow("res1", res_img)
-    new = (res_img * (255/np.max(res_img))).astype(np.uint8)
-    cv2.imshow("res",new)
-
-    """# параметры для банка фильтров
-    w = 0.4
-    sigma = 0.5
-    list_w_t0 = [0.15, 0.3, 0.45]
-    list_theta = [0, np.pi/3, 2*np.pi/3]
-    bank_filter = []
-
-    # создание банка фильтров
-    for w_t0 in list_w_t0:
-        for theta in list_theta:
-            gabor_filter = Gabor3D(size=n, theta=theta, w_t0=w_t0, w=w, sigma=sigma)
-            bank_filter.append(gabor_filter)
-
-    show_filter3D(bank_filter[2].odd_filter, "y")
-
-    # применяем банк фильтров
-    list_res_img = []
-    for gabor_filter in bank_filter:
+    while True:
         res_img = gabor_filter.filtered(block_img)
-        list_res_img.append(res_img)
 
-    names = [f"theta_{j+1}, w_t0_{i+1}" for i in range(3) for j in range(3)]
-    show_images(list_res_img, names, 3)
+        print([np.min(res_img), np.max(res_img)], res_img.dtype)
+        cv2.imshow("res", res_img)
+        new = (res_img * (255 / np.max(res_img))).astype(np.uint8)
+        cv2.imshow("lin_res", new)
 
-    #cv2.imshow("1", list_res_img[1])"""
+        # обновляем блок изб
+        ret, img = cap.read()
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        block_img = np.concatenate((np.delete(block_img, 0, 0), gray_img[None, :]))
 
-    if cv2.waitKey(0) == ord('q'):
-        pass
+        """# параметры для банка фильтров
+        w = 0.4
+        sigma = 0.5
+        list_w_t0 = [0.15, 0.3, 0.45]
+        list_theta = [0, np.pi/3, 2*np.pi/3]
+        bank_filter = []
+    
+        # создание банка фильтров
+        for w_t0 in list_w_t0:
+            for theta in list_theta:
+                gabor_filter = Gabor3D(size=n, theta=theta, w_t0=w_t0, w=w, sigma=sigma)
+                bank_filter.append(gabor_filter)
+    
+        show_filter3D(bank_filter[2].odd_filter, "y")
+    
+        # применяем банк фильтров
+        list_res_img = []
+        for gabor_filter in bank_filter:
+            res_img = gabor_filter.filtered(block_img)
+            list_res_img.append(res_img)
+    
+        names = [f"theta_{j+1}, w_t0_{i+1}" for i in range(3) for j in range(3)]
+        show_images(list_res_img, names, 3)
+    
+        #cv2.imshow("1", list_res_img[1])"""
 
+        k = cv2.waitKey(1)
+
+        if k == ord('w'):
+            pass
+
+        if k == ord('q'):
+            break
 
 
 if __name__ == '__main__':
